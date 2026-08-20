9cf45ff57745ea261068a55bb1e2849485bf47c5
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-08-20 19:05:54 +0900

    Create access.js

---
+++ liar-game/js/views/access.js
@@ -0,0 +1,2 @@
+import { LOGIN_URL } from "../config.js";
+export const accessView=()=>`<h1 class="brand">🎭 LIAR GAME</h1><section class="card stack"><h2>로그인이 필요합니다</h2><p>로그인이 필요한 기능입니다.</p><a class="button" href="${LOGIN_URL}">로그인 페이지로 이동</a></section>`;
