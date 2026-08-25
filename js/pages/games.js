3bbf5ec922d36fb3b33e22f9dd62d816ead5860a
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-08-25 14:36:33 +0900

    feat: add splendor to game hub

--- js/pages/games.js
+++ js/pages/games.js
@@ -8,6 +8,13 @@ const GAMES = [
     href: "./liar-game/",
     buttonText: "라이어 게임 시작",
   },
+  {
+    icon: "💎",
+    title: "스플렌더",
+    description: "보석을 모아 개발 카드를 구매하고 영구 보너스를 쌓는 전략 게임이에요.",
+    href: "./splendor-game/",
+    buttonText: "스플렌더 UI 데모",
+  },
 ];
 
 export function renderGames() {
