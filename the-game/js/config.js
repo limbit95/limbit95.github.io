768f87ef9770a8abb9abfd0bd5ff6e9ac6d4bd5d
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-08-28 09:55:11 +0900

    feat(the-game): share community Supabase config

---
+++ the-game/js/config.js
@@ -0,0 +1,4 @@
+export { SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY } from "../../js/config.js";
+
+export const LOGIN_URL = new URL("../../#/login", import.meta.url).href;
+export const GAMES_URL = new URL("../../#/games", import.meta.url).href;
