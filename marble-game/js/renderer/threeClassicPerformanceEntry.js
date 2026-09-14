193b5a87408e1cd8d2dd73f128f02e3de7daa4ad
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-09-14 16:43:25 +0900

    perf(marble): add cache-safe Classic performance entry

---
+++ marble-game/js/renderer/threeClassicPerformanceEntry.js
@@ -0,0 +1,6 @@
+import * as THREE from "three";
+import { installClassicShadowUpdatePolicy } from "./threeClassicPrototypeDiagnostics.js?v=20260914-r14";
+
+installClassicShadowUpdatePolicy(THREE);
+
+export * from "./threeClassicMoneyPresentation.js?v=20260912-r21";
