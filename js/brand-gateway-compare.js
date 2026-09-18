e0c09c4e7d9d13189c4a3fdbdff6f745791bb597
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-09-18 21:37:11 +0900

    chore: remove rejected AF AG AH gateway variants

--- js/brand-gateway-compare.js
+++ js/brand-gateway-compare.js
@@ -53,24 +53,6 @@ export const BRAND_GATEWAY_VARIANTS = Object.freeze([
     branch: "feature/brand-gateway-ae-20260918",
     commit: "48fcb21e00a3d636baf39c6327be74a4e9c74834",
   },
-  {
-    id: "gateway-af",
-    label: "AF",
-    branch: "feature/brand-gateway-af-20260918",
-    commit: "40e7b03f33220c0555323592e21c62620f496841",
-  },
-  {
-    id: "gateway-ag",
-    label: "AG",
-    branch: "feature/brand-gateway-ag-20260918",
-    commit: "a75138b90bebb976f05cbba260d79ce888096086",
-  },
-  {
-    id: "gateway-ah",
-    label: "AH",
-    branch: "feature/brand-gateway-ah-20260918",
-    commit: "54dab3805a3c43f13ec6d02dd43f45fdb1bc1373",
-  },
 ]);
 
 const RAW_ROOT = "https://raw.githubusercontent.com/limbit95/limbit95.github.io";
