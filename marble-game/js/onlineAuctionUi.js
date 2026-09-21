d30c10f4325622689efcade6e8b554a2280ade05
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-09-21 21:42:07 +0900

    fix: bump online Auction presentation stylesheet

--- marble-game/js/onlineAuctionUi.js
+++ marble-game/js/onlineAuctionUi.js
@@ -136,7 +136,7 @@ function ensureAuctionStyles(documentObject) {
   if (documentObject.querySelector("link[data-online-auction-style]")) return;
   const link = documentObject.createElement("link");
   link.rel = "stylesheet";
-  link.href = new URL("../css/auction-ui.css?v=20260919-r3", import.meta.url).href;
+  link.href = new URL("../css/auction-ui.css?v=20260921-r4", import.meta.url).href;
   link.dataset.onlineAuctionStyle = "true";
   documentObject.head.append(link);
 }
