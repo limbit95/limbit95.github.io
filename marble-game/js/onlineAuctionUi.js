09f1368fbfc1ff6416b6c28bcf316440361cb11b
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-09-21 23:33:15 +0900

    chore: bump Marble auction style cache

--- marble-game/js/onlineAuctionUi.js
+++ marble-game/js/onlineAuctionUi.js
@@ -145,7 +145,7 @@ function ensureAuctionStyles(documentObject) {
   if (documentObject.querySelector("link[data-online-auction-style]")) return;
   const link = documentObject.createElement("link");
   link.rel = "stylesheet";
-  link.href = new URL("../css/auction-ui.css?v=20260921-r4", import.meta.url).href;
+  link.href = new URL("../css/auction-ui.css?v=20260921-r5", import.meta.url).href;
   link.dataset.onlineAuctionStyle = "true";
   documentObject.head.append(link);
 }
