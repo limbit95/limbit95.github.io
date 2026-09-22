7fd5ef60e51858db28ff276dc65d9359f3912884
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-09-22 20:26:46 +0900

    feat: define Marble auction start sequence timing

--- marble-game/js/core/auction.js
+++ marble-game/js/core/auction.js
@@ -25,6 +25,8 @@ function requirePlayer(players, playerId) {
 
 export const AUCTION_TIMING = Object.freeze({
   voteMs: 15_000,
+  startNoticeMs: 2_400,
+  rouletteMs: 3_200,
   bidTurnMs: 15_000,
 });
 
