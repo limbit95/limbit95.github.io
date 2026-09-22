a6e1b86ad350d19021541874f71588a495f56e6c
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-09-23 00:36:56 +0900

    chore: refresh Marble online session for avatar selector

--- marble-game/js/onlineGameController2d.js
+++ marble-game/js/onlineGameController2d.js
@@ -1,6 +1,6 @@
 import { GAME_STATUS } from "./core/gameEngine.js";
 import { TURN_PHASES } from "./core/turnMachine.js";
-import { createOnlineClassicSession, isOnlineViewerTurn } from "./onlineSession.js?v=20260919-r13";
+import { createOnlineClassicSession, isOnlineViewerTurn } from "./onlineSession.js?v=20260923-r1";
 import { markOnlineVisualRuntime } from "./onlineVisualPolicy.js?v=20260910-r10";
 import { CLASSIC_RULES } from "./themes/classic/rules.js";
 import { formatThemeMoney } from "./themes/money.js";
