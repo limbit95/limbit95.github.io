120fbe2cc00f7625229c9e77ce04711f08115d77
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-09-22 22:58:18 +0900

    fix: set The Game default slider and output volume

--- js/game-audio/bgmCatalog.js
+++ js/game-audio/bgmCatalog.js
@@ -11,8 +11,8 @@ const GAME_BGM_CATALOG = Object.freeze({
     licenseUrl: "https://creativecommons.org/licenses/by/4.0/",
     attribution: '"Invariance" Kevin MacLeod (incompetech.com)',
     modified: false,
-    defaultVolume: 0.22,
-    defaultOutputMultiplier: 2,
+    defaultVolume: 0.7,
+    defaultOutputVolume: 0.6,
     loop: true,
   }),
 });
