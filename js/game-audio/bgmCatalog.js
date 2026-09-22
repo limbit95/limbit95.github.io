86fb736978e9ea681fc9b6f99ac918b20f15d7ef
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-09-22 23:30:43 +0900

    feat: register Deadly Roulette for Liar Game

--- js/game-audio/bgmCatalog.js
+++ js/game-audio/bgmCatalog.js
@@ -1,4 +1,20 @@
 const GAME_BGM_CATALOG = Object.freeze({
+  "liar-game": Object.freeze({
+    gameId: "liar-game",
+    src: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Deadly%20Roulette.mp3",
+    title: "Deadly Roulette",
+    artist: "Kevin MacLeod",
+    isrc: "USUAN1600033",
+    sourceUrl: "https://incompetech.com/music/royalty-free/index.html?Search=Search&isrc=USUAN1600033",
+    previewUrl: "https://www.youtube.com/watch?v=Hnbv_KNxVo8",
+    license: "CC BY 4.0",
+    licenseUrl: "https://creativecommons.org/licenses/by/4.0/",
+    attribution: '"Deadly Roulette" Kevin MacLeod (incompetech.com)',
+    modified: false,
+    defaultVolume: 0.35,
+    defaultOutputVolume: 0.35,
+    loop: true,
+  }),
   "the-game": Object.freeze({
     gameId: "the-game",
     src: "https://incompetech.com/music/royalty-free/mp3-royaltyfree/Invariance.mp3",
