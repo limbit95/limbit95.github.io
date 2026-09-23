82508e762946030fb305be6b61a6d9c88c70cbad
임성혁 <111622452+limbit95@users.noreply.github.com>
2026-09-23 12:46:53 +0900

    fix: bust Marble online boot cache after session sync

--- marble-game/js/playWindow.js
+++ marble-game/js/playWindow.js
@@ -6,7 +6,7 @@ const ONLINE_ROOM_QUERY_KEY = "onlineRoom";
 const ONLINE_VISUAL_QUERY_KEY = "marbleVisuals";
 const CLASSIC_PLAY_MODE = "classic";
 const PLAY_WINDOW_NAME = "marbleClassicPlay";
-const ONLINE_BOOT_REVISION = "20260923-r2";
+const ONLINE_BOOT_REVISION = "20260923-r3";
 const ONLINE_BOOT_TIMEOUT_MS = 8000;
 
 export function createClassicPlayUrl(href) {
